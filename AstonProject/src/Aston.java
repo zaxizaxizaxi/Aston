a5739e55a42a468594728c9947b2f7564265029b
zaxizaxizaxi <Zaxisnegzaxisnus@gmail.com>
2023-11-16 23:27:13 +0300

    FirstLessonCommit

--- AstonProject/src/Aston.java
+++ AstonProject/src/Aston.java
@@ -1,2 +1,43 @@
 public class Aston {
+
+    public static void main(String[] args){
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple\n");
+    }
+    public static void checkSumSign() {
+        int a = 5;
+        int b = -7;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная\n");
+        } else {
+            System.out.println("Сумма отрицательная\n");
+        }
+    }
+    public static void printColor() {
+        int value = 52;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый\n");
+        } else {
+            System.out.println("Зеленый\n");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 200;
+        int b = 400;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
 }
